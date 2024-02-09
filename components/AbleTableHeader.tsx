5c61a4f275c6f1bd08230d9d0eaacd81c0021ce1
Phil Wonder <phil@noblecode.dev>
2024-02-09 11:38:06 -0500

    add th and tfoot components

--- components/AbleTableHeader.tsx
+++ components/AbleTableHeader.tsx
@@ -5,19 +5,19 @@ import { getField } from "../utilities/nestedFieldHelpers";
 import { AbleStyles } from "../types/AbleStyles";
 import React from "react";
 
-type AbleTableCellProps<T extends object> = {
+type AbleTableHeaderProps<T extends object> = {
   data: T & { key: string | number };
   column: KeyedColumn<T>;
   index: number;
   styles: AbleStyles<T> | undefined;
 };
 
-export function AbleTableCellComponent<T extends object>({
+export function AbleTableHeaderComponent<T extends object>({
   data,
   column,
   index,
   styles,
-}: AbleTableCellProps<T>) {
+}: AbleTableHeaderProps<T>) {
   return (
     <td
       onClick={(e) => {
@@ -48,4 +48,6 @@ export function AbleTableCellComponent<T extends object>({
   );
 }
 
-export const AbleTableCell = memo(AbleTableCellComponent) as typeof AbleTableCellComponent;
+export const AbleTableHeader = memo(
+  AbleTableHeaderComponent
+) as typeof AbleTableHeaderComponent;
